9fa50084ec2d00c19789ad30d32b63f1711ae6ac
Nishant Neeraj <nishant.neeraj@gmail.com>
2019-02-20 13:53:37 +0530

    😎 using Google OCR too!

--- src/App.js
+++ src/App.js
@@ -64,7 +64,12 @@ class App extends Component {
       pixelCrop.height
     );
 
-    this.setState({ croppedCanvas: canvas });
+    const croppedImageBase64 = canvas
+      .toDataURL("image/png")
+      .replace(/^data:image\/(png|jpg);base64,/, "");
+
+    this.setState({ croppedCanvas: canvas, croppedImageBase64 });
+
     return new Promise((resolve, reject) => {
       canvas.toBlob(blob => {
         if (!blob) {
@@ -81,24 +86,27 @@ class App extends Component {
 
   onCropChange = crop => this.setState({ crop });
 
-  recognize = name => {
-    const { fields } = this.state;
+  recognize = async name => {
+    const { fields, useGoogleOCR } = this.state;
     const expectedRaw = fields[name];
     console.log("fields", fields);
     const expected = expectedRaw && expectedRaw.trim().toLowerCase();
     this.setState({ isProcessing: true });
-    // eslint-disable-next-line
-    Tesseract.recognize(this.state.croppedCanvas).then(txtObj => {
-      const txt = txtObj.text;
-      const txtLower = txt && txt.trim().toLowerCase();
-      console.log(`xtt: ${txtLower} === ${expected}`);
-      this.setState({ txt, isProcessing: false });
-      if (txtLower === expected) {
-        window.alert(`✔️ ${name} entered right!`);
-      } else {
-        window.alert(`❌ ${name} entered wrong!!`);
-      }
-    });
+    let ocrTxt = "";
+    if (useGoogleOCR) {
+      ocrTxt = await this.readUsingGoogleOCR();
+    } else {
+      ocrTxt = await this.readUsingTesseract();
+    }
+
+    const txtLower = ocrTxt && ocrTxt.trim().toLowerCase();
+    console.log(`xtt: ${txtLower} === ${expected}`);
+    this.setState({ txt: ocrTxt, isProcessing: false });
+    if (txtLower === expected) {
+      window.alert(`✔️ ${name} entered right!`);
+    } else {
+      window.alert(`❌ ${name} entered wrong!!`);
+    }
   };
 
   setField = (name, event) => {
@@ -109,8 +117,68 @@ class App extends Component {
     fields[name] = value;
     this.setState({ fields });
   };
+
+  readUsingTesseract = async () => {
+    // eslint-disable-next-line
+    const readJSON = await Tesseract.recognize(this.state.croppedCanvas);
+    return readJSON.text.trim();
+  };
+
+  readUsingGoogleOCR = async () => {
+    const { croppedImageBase64 } = this.state;
+    if (!croppedImageBase64) {
+      return "NO IMAGE SELECTED!";
+    }
+
+    const key = "AIzaSyBNc4UwRVXbfJ0FK56jvG7uFlPuDZ1T1HQ";
+    const body = JSON.stringify({
+      requests: [
+        {
+          image: {
+            content: croppedImageBase64
+          },
+          features: [
+            {
+              type: "TEXT_DETECTION"
+            }
+          ]
+        }
+      ]
+    });
+
+    const ocrJSON = await fetch(
+      `https://vision.googleapis.com/v1/images:annotate?key=${key}`,
+      {
+        method: "POST",
+        body
+      }
+    ).then(async res => {
+      if (res.status < 200 || res.status > 299) {
+        console.error(
+          "Something went wrong: " + JSON.stringify(await res.json())
+        );
+        throw Error(`something went wrong with GoogleOCR`);
+      }
+      return res.json();
+    });
+
+    return ocrJSON.responses[0].fullTextAnnotation.text.trim();
+  };
+
+  changeOCR = e => {
+    console.log(e.target.name + " = " + e.target.value);
+    this.setState({ useGoogleOCR: !this.state.useGoogleOCR });
+  };
+
   render() {
-    const { src, crop, croppedImageUrl, txt, isProcessing } = this.state;
+    const {
+      src,
+      crop,
+      croppedImageUrl,
+      txt,
+      isProcessing,
+      useGoogleOCR
+    } = this.state;
     return (
       <LoadingOverlay
         active={isProcessing}
@@ -119,6 +187,23 @@ class App extends Component {
       >
         <div className="wrapper">
           <h1>TEH P@SSBOOK VERIFIER</h1>
+          <div>
+            <label>USE:&nbsp;</label>
+            <input
+              type="radio"
+              checked={!useGoogleOCR}
+              onChange={this.changeOCR}
+              name="tes"
+            />
+            Tesseract |
+            <input
+              type="radio"
+              checked={!!useGoogleOCR}
+              onChange={this.changeOCR}
+              name="goo"
+            />{" "}
+            Google OCR
+          </div>
           <div className="App">
             <div>
               <div>
@@ -156,7 +241,8 @@ class App extends Component {
                       type="text"
                       onChange={event => this.setField(name, event)}
                     />
-                    <button onClick={() => this.recognize(name)}>
+                    <button onClick={async () => this.recognize(name)}>
+                      {/* <button onClick={() => this.fetchFromGoogleOCR()}> */}
                       validate this
                     </button>
                   </div>
